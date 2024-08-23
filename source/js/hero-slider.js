023bcf23dd1792a41a7374596a015b198ed81256
iki_welt <iki.welt@ya.ru>
2024-08-23 22:40:57 +0300

    add hero swiper

--- source/js/hero-slider.js
+++ source/js/hero-slider.js
@@ -1,16 +1,31 @@
 import { Swiper } from './vendor/swiper';
-// import { Pagination } from './vendor/modules';
+import { Pagination } from './vendor/modules';
 
-const promoSlider = new Swiper ('.hero__swiper', {
-//   modules: [ Pagination ],
+const promoSlider = new Swiper('.hero__swiper', {
+  modules: [Pagination],
   direction: 'horizontal',
-  loop: 'true',
-  slidesPerView: 'auto',
-//   pagination: {
-//     el: '.hero__pagination',
-//     type: 'bullets',
-//     clickable: true,
-//   },
+  slidesPerView: 1,
+  loop: true,
+  breakpoints: {
+    320: {
+      pagination: false,
+      allowTouchMove: true,
+    },
+    768: {
+      pagination: false,
+      allowTouchMove: true,
+    },
+    1440: {
+      pagination: {
+        el: '.hero__pagination',
+        bulletClass: 'hero__pagination-bullet',
+        bulletActiveClass: 'hero__pagination-bullet--is-active',
+        type: 'bullets',
+        clickable: true,
+      },
+      allowTouchMove: false,
+    },
+  },
 });
 
 export { promoSlider };
