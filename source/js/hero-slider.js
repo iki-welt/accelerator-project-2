6d1dbf93527b16cb909dd398cf4e887293bf7201
iki_welt <iki.welt@ya.ru>
2024-09-02 03:58:04 +0300

    pp mobile

--- source/js/hero-slider.js
+++ source/js/hero-slider.js
@@ -6,23 +6,21 @@ const promoSlider = new Swiper('.hero__swiper', {
   direction: 'horizontal',
   slidesPerView: 1,
   loop: true,
+  pagination: {
+    el: '.hero__pagination',
+    bulletClass: 'hero__pagination-bullet',
+    bulletActiveClass: 'hero__pagination-bullet--is-active',
+    type: 'bullets',
+    clickable: true,
+  },
   breakpoints: {
     320: {
-      pagination: false,
       allowTouchMove: true,
     },
     768: {
-      pagination: false,
       allowTouchMove: true,
     },
     1440: {
-      pagination: {
-        el: '.hero__pagination',
-        bulletClass: 'hero__pagination-bullet',
-        bulletActiveClass: 'hero__pagination-bullet--is-active',
-        type: 'bullets',
-        clickable: true,
-      },
       allowTouchMove: false,
     },
   },
